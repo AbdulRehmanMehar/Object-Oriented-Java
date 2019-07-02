b04e2f685dd94fbd2d1820416d4267e5a181280f
Abdul Rehman <mehars.6925@gmail.com>
2019-07-02 17:20:34 +0500

    + box properties and methods

--- Box.java
+++ Box.java
@@ -1,3 +1,35 @@
 public class Box extends Shape {
-    
+    private double width;
+    private double height;
+
+    public Box(double width, double height, String color, int positionX, int positionY) {
+        super(color, positionX, positionY);
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Width: " + this.getWidth() + " Height: " + this.getHeight() + " Color: " + super.getColor() + " Position: (" + super.getPositionX() + ", " + super.getPositionY() + ")");
+    }
+
+    @Override
+    public String getType() {
+        return "Box";
+    }
+
 }
