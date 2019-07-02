6f2095c404747c529a657db9799f7a58c3a80c5e
Abdul Rehman <mehars.6925@gmail.com>
2019-07-02 17:39:08 +0500

    + canvas methods

--- Canvas.java
+++ Canvas.java
@@ -1,3 +1,29 @@
+import java.util.Iterator;
+import java.util.ArrayList;
+
 public class Canvas {
-    
+    private ArrayList<Shape> shapes;
+
+    public Canvas() {
+        this.shapes = new ArrayList<Shape>();
+    }
+
+    public Canvas(ArrayList<Shape> shapes) {
+        this.shapes = shapes;
+    }
+
+    public void addShape(Shape shape) {
+        this.shapes.add(shape);
+    }
+
+    public void draw() {
+        Iterator<Shape> itr = this.shapes.iterator();
+        while(itr.hasNext()) {
+            Shape shape = itr.next();
+            System.out.println(shape.getType());
+            shape.draw();
+            System.out.println();
+        }
+    }
+
 }
