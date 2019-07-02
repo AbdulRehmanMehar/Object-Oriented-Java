4f3fa2cc94320d7de49c7a096bf7cb254d622138
Abdul Rehman <mehars.6925@gmail.com>
2019-07-02 17:12:30 +0500

    applied generalization

--- Shape.java
+++ Shape.java
@@ -1,3 +1,40 @@
 public abstract class Shape {
+    private String color;
+    private int positionX;
+    private int positionY;
+
+    public Shape(String color, int positionX, int positionY) {
+        this.setColor(color);
+        this.setPositionX(positionX);
+        this.setPositionY(positionY);
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getPositionX() {
+        return this.positionX;
+    }
+
+    public int getPositionY() {
+        return this.positionY;
+    }
+
+    public abstract void draw();
+
+    public abstract String getType();
 
 }
