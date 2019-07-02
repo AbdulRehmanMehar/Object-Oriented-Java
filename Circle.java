ffe595ff6b50b17f6379cb615f9d4f9b34f9e71b
Abdul Rehman <mehars.6925@gmail.com>
2019-07-02 17:20:48 +0500

    + circle properties and methods

--- Circle.java
+++ Circle.java
@@ -1,3 +1,28 @@
 public class Circle extends Shape {
-    
+    private double radius;
+
+    public Circle(double radius, String color, int positionX, int positionY) {
+        super(color, positionX, positionY);
+        this.setRadius(radius);
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Radius: " + this.getRadius() + " Color: " + super.getColor()
+                + " Position: (" + super.getPositionX() + ", " + super.getPositionY() + ")");
+    }
+
+    @Override
+    public String getType() {
+        return "Circle";
+    }
+
 }
